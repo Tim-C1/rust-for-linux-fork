7caa7b7826db429a39f675811246936cb308ab7f
CTR <chentingrun1011@163.com>
2023-07-17 13:08:41 +0800

    compile scull module

---
+++ samples/rust/rust_scull.rs
@@ -0,0 +1,94 @@
+//! Scull module in Rust.
+use kernel::io_buffer::{IoBufferReader, IoBufferWriter};
+use kernel::prelude::*;
+use kernel::sync::{Mutex, Arc, ArcBorrow};
+use kernel::{file, miscdev};
+
+module! {
+    type: Scull,
+    name: "scull",
+    license: "GPL",
+    params: {
+        nr_devs: u32 {
+            default: 1,
+            permissions: 0o644,
+            description: "Number of scull devices",
+        },
+    },
+}
+
+struct Device {
+    number: usize,
+    contents: Mutex<Vec<u8>>,
+}
+
+struct Scull {
+    _devs: Vec<Pin<Box<miscdev::Registration<Scull>>>>,
+}
+
+#[vtable]
+impl file::Operations for Scull {
+    type OpenData = Arc<Device>;
+    type Data = Arc<Device>;
+
+    fn open(context: &Arc<Device>, file: &file::File) -> Result<Arc<Device>> {
+        pr_info!("File for device {} was opened\n", context.number);
+        if file.flags() & file::flags::O_ACCMODE == file::flags::O_WRONLY {
+            context.contents.lock().clear();
+        }
+        Ok(context.clone())
+    }
+
+    fn read(
+        data: ArcBorrow<'_, Device>,
+        _file: &file::File,
+        writer: &mut impl IoBufferWriter,
+        offset: u64,
+    ) -> Result<usize> {
+        pr_info!("File for device {} was read\n", data.number);
+        let offset = offset.try_into()?;
+        let vec = data.contents.lock();
+        let len = core::cmp::min(writer.len(), vec.len().saturating_sub(offset));
+        writer.write_slice(&vec[offset..][..len])?;
+        Ok(len)
+    }
+
+    fn write(
+        data: ArcBorrow<'_, Device>,
+        _file: &file::File,
+        reader: &mut impl IoBufferReader,
+        offset: u64,
+    ) -> Result<usize> {
+        pr_info!("File for device {} was written\n", data.number);
+        let offset = offset.try_into()?;
+        let len = reader.len();
+        let new_len = len.checked_add(offset).ok_or(EINVAL)?;
+        let mut vec = data.contents.lock();
+        if new_len > vec.len() {
+            vec.try_resize(new_len, 0)?;
+        }
+        reader.read_slice(&mut vec[offset..][..len])?;
+        Ok(len)
+    }
+}
+
+impl kernel::Module for Scull {
+    fn init(_name: &'static CStr, module: &'static ThisModule) -> Result<Self> {
+        let count = {
+            let lock = module.kernel_param_lock();
+            (*nr_devs.read(&lock)).try_into()?
+        };
+        pr_info!("Hello world, {} devices!\n", count);
+        let mut devs = Vec::try_with_capacity(count)?;
+        for i in 0..count {
+            let dev = Arc::try_new(Device {
+                number: i,
+                contents: unsafe { Mutex::new(Vec::new()) },
+            })?;
+            let reg = miscdev::Registration::new_pinned(fmt!("scull{i}"), dev)?;
+            devs.try_push(reg)?;
+        }
+        Ok(Self { _devs: devs })
+    }
+}
+
